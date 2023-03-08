79067863c3c2526d0ab7176660e395b94399c69d
Bhagyodaya-Varshney <126950178+Bhagyodaya-Varshney@users.noreply.github.com>
2023-03-08 19:04:00 +0530

    Add files via upload

---
+++ Valintine_Number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Valintine_Number{
+    public static void main(String[]args){
+        System.out.print("Enter 1 No:");
+        Scanner st = new Scanner(System.in);
+        int sc=st.nextInt();
+        System.out.print("Enter 2 No:");
+        Scanner sb = new Scanner(System.in);
+        int sa=sb.nextInt();
+        //if((sc+sa)==(sc^sa)){
+        //        System.out.print("Entered No. is Valintine No.");
+        //    }
+        //else{
+        //    System.out.print("Entered No. is not Valintine No.");
+        //}
+        System.out.print(x+y);
+        System.out.print(x^y);
+    }
+}
